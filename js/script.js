eba941d9fd7375f4f0ec19f677c3a920ec147e9d
AlexBieg <alexbieg95@gmail.com>
2015-11-10 17:15:33 -0800

    added custom playlist names

--- js/script.js
+++ js/script.js
@@ -44,8 +44,7 @@ app.controller('controller', function($scope, $http, Spotify) {
 	}
 
 	$scope.addPlaylist = function() {
-		var playlistName = "test";
-		Spotify.createPlaylist($scope.username, {name : playlistName}).then(function(playlist) {
+		Spotify.createPlaylist($scope.username, {name : $scope.playlistName}).then(function(playlist) {
 			var uris = []
 			for(var i = 0; i < $scope.tracks.length; i++) {
 				uris.push($scope.tracks[i].uri);
